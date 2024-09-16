8dbe20b3417ded7ac9dedce096c3a101132e2e03
dalton <dakribe94@gmail.com>
2024-09-16 16:13:50 -0500

    bootstrap server

--- go.mod
+++ go.mod
@@ -1,3 +1,10 @@
 module github.com/d4499/jager
 
 go 1.23.0
+
+require (
+	github.com/go-chi/chi/v5 v5.1.0
+	github.com/go-chi/render v1.0.3
+)
+
+require github.com/ajg/form v1.5.1 // indirect
--- main.go
+++ main.go
@@ -1,7 +1,51 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/render"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	run()
+}
+
+func run() error {
+	srv := newServer(serverConfig{
+		addr: ":8080",
+	})
+
+	err := srv.ListenAndServe()
+	if err != nil {
+		log.Fatalf("Unable to start server: %s", err)
+	}
+
+	return nil
+}
+
+type serverConfig struct {
+	addr string
+}
+
+func newServer(conf serverConfig) *http.Server {
+	r := chi.NewRouter()
+	r.Use(render.SetContentType(render.ContentTypeJSON))
+
+	r.Use(middleware.Logger)
+
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("hello 123"))
+	})
+
+	return &http.Server{
+		Addr:         conf.addr,
+		Handler:      r,
+		IdleTimeout:  time.Minute,
+		WriteTimeout: time.Second * 30,
+		ReadTimeout:  time.Second * 10,
+	}
 }
