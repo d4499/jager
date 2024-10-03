809684fd23586688f38db993af627f4a7a60e124
dalton <dakribe94@gmail.com>
2024-10-03 01:57:28 -0500

    scaffold web

---
+++ web/src/App.tsx
@@ -0,0 +1,28 @@
+import { createSignal } from "solid-js";
+import "./App.css";
+
+function App() {
+	const [count, setCount] = createSignal(0);
+
+	return (
+		<>
+			<div>
+				<h1>Hello Jager!</h1>
+			</div>
+			<h1>Vite + Solid</h1>
+			<div class="card">
+				<button onClick={() => setCount((count) => count + 1)}>
+					count is {count()}
+				</button>
+				<p>
+					Edit <code>src/App.tsx</code> and save to test HMR
+				</p>
+			</div>
+			<p class="read-the-docs">
+				Click on the Vite and Solid logos to learn more
+			</p>
+		</>
+	);
+}
+
+export default App;
