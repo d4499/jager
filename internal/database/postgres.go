8323a8c37e848b1ac099ce7c0c196c02a942121a
dalton <dakribe94@gmail.com>
2024-10-04 06:55:48 -0500

    ping postgres on connection

--- internal/database/postgres.go
+++ internal/database/postgres.go
@@ -13,5 +13,11 @@ func NewPostgres(conn string) *pgxpool.Pool {
 		log.Fatalf("Unable to connect to postgres: %v", err)
 	}
 
+	err = pool.Ping(context.Background())
+	if err != nil {
+		log.Fatalf("Unable to ping database: %v", err)
+	}
+	log.Println("successfully connected to postgres")
+
 	return pool
 }
