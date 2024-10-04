57fe2708cd54daa2b1854b0a5e4b46c51fdb9676
dalton <dakribe94@gmail.com>
2024-10-04 05:52:29 -0500

    login form

--- web/src/pages/login.tsx
+++ web/src/pages/login.tsx
@@ -1,7 +1,41 @@
+import { createForm, SubmitHandler, valiForm } from "@modular-forms/solid";
+import { Match, Switch } from "solid-js";
+import * as v from "valibot";
+
+const LoginSchema = v.object({
+	email: v.pipe(
+		v.string(),
+		v.nonEmpty("Please enter your email."),
+		v.email("Please enter a valid email"),
+		v.maxLength(320),
+	),
+});
+
+type LoginForm = v.InferInput<typeof LoginSchema>;
+
 export function Login() {
+	const [loginForm, { Form, Field }] = createForm<LoginForm>({
+		validate: valiForm(LoginSchema),
+	});
+
+	const handleSubmit: SubmitHandler<LoginForm> = (values) => {
+		console.log(values);
+	};
+
 	return (
 		<div>
 			<h1>Login</h1>
+			<Form onSubmit={handleSubmit}>
+				<Field name="email">
+					{(field, props) => (
+						<>
+							<input {...props} type="email" />
+							{field.error && <div>{field.error}</div>}
+						</>
+					)}
+				</Field>
+				<button type="submit">Continue with Email</button>
+			</Form>
 		</div>
 	);
 }
