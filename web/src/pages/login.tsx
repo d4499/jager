79ac56dbd5a576d8713bf8c513e1ab5b58097c08
dalton <dakribe94@gmail.com>
2024-10-15 18:11:49 -0500

    fix login flow styling

--- web/src/pages/login.tsx
+++ web/src/pages/login.tsx
@@ -7,6 +7,7 @@ import {
 	TextFieldLabel,
 	TextFieldInput,
 } from "../components/ui/text-field";
+import { createSignal, Match, Switch } from "solid-js";
 
 const LoginSchema = v.object({
 	email: v.pipe(
@@ -20,31 +21,44 @@ const LoginSchema = v.object({
 type LoginForm = v.InferInput<typeof LoginSchema>;
 
 export function Login() {
+	const [sent, setSent] = createSignal(false);
 	const { sendMagicLink } = useAuth();
-	const [loginForm, { Form, Field }] = createForm<LoginForm>({
+	const [_, { Form, Field }] = createForm<LoginForm>({
 		validate: valiForm(LoginSchema),
 	});
 
 	const handleSubmit: SubmitHandler<LoginForm> = (values) => {
-		console.log(values);
 		sendMagicLink(values.email);
+		setSent(true);
 	};
 
 	return (
 		<div>
 			<h1>Login</h1>
-			<Form onSubmit={handleSubmit}>
-				<Field name="email">
-					{(field, props) => (
-						<TextField class="grid w-full max-w-sm items-center gap-2">
-							<TextFieldLabel for="email">Email</TextFieldLabel>
-							<TextFieldInput {...props} type="email" />
-							{field.error && <div class="text-red-500">{field.error}</div>}
-						</TextField>
-					)}
-				</Field>
-				<Button type="submit">Continue with Email</Button>
-			</Form>
+			<Switch
+				fallback={
+					<>
+						<Form onSubmit={handleSubmit}>
+							<Field name="email">
+								{(field, props) => (
+									<TextField class="grid w-full max-w-sm items-center gap-2">
+										<TextFieldLabel for="email">Email</TextFieldLabel>
+										<TextFieldInput {...props} type="email" />
+										{field.error && (
+											<div class="text-red-500">{field.error}</div>
+										)}
+									</TextField>
+								)}
+							</Field>
+							<Button type="submit">Continue with Email</Button>
+						</Form>
+					</>
+				}
+			>
+				<Match when={sent()}>
+					<p>Please check your email</p>
+				</Match>
+			</Switch>
 		</div>
 	);
 }
